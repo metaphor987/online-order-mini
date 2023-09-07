b780adf5da80f1347eb9635522cb6108d869aaa3
huijieliu8 <91745551+metaphor987@users.noreply.github.com>
2023-09-07 13:48:32 -0400

    set up express.js mysql, use postman to test get and post

---
+++ backend/index.js
@@ -0,0 +1,47 @@
+import express from "express"
+import mysql from "mysql"
+import dotenv from "dotenv"
+
+const app = express()
+
+dotenv.config();
+
+const db = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password: process.env.MYSQL_PASSWORD,
+    database:"test"
+})
+
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.json("hello, this is the backend")
+})
+
+app.get("/books", (req, res) => {
+    const q = "SELECT * FROM test.books;"
+    db.query(q, (err, data) => {
+        if (err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+// CRUD operations
+app.post("/books", (req, res) => {
+    const q = "INSERT INTO books (`title`, `desc`, `cover`) VALUES (?)"
+    const values = [
+        req.body.title,
+        req.body.desc,
+        req.body.cover 
+    ]
+
+    db.query(q, [values], (err, data) => {
+        if (err) return res.json(err)
+        return res.json("books are created successfully")
+    })
+})
+
+app.listen(8000, () => {
+    console.log("Connected to backend!")
+})
